aea070d96fcb9bc12d986676453c092a95f167a8
Carl Hamilton <carlanthonyhamilton@gmail.com>
2014-11-24 20:35:27 +0000

    Package updates. Node-sass added. Gitignore updates.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,84 +2,83 @@
 
 module.exports = function(grunt) {
 
-	// Project configuration
-	grunt.initConfig({
+  // Project configuration
+  grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-		// Compile Sass
+    // Compile Sass
 
-		// https://github.com/gruntjs/grunt-contrib-sass
-		// https://npmjs.org/package/grunt-contrib-sass
+    // https://github.com/sindresorhus/grunt-sass
+    // https://npmjs.org/package/grunt-sass
 
-		sass: {
-			options: {
-				precision: 9,
-				style: 'nested',
-				unixNewlines: true
-			},
-			all: {
-				files: [{
-					expand: true,
-					cwd: 'scss',
-					src: ['**/*.scss'],
-					dest: 'css',
-					ext: '.css'
-				}]
-			}
-		},
-
-
-		// Minify CSS
-
-		// https://github.com/gruntjs/grunt-contrib-cssmin
-		// https://npmjs.org/package/grunt-contrib-cssmin
-
-		cssmin: {
-			all: {
-				files: [{
-					expand: true,
-					cwd: 'css',
-					src: ['**/*.css'],
-					dest: 'css',
-					ext: '.css'
-				}]
-			}
-		},
+    sass: {
+      options: {
+        precision: 9,
+        style: 'nested',
+        unixNewlines: true
+      },
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['**/*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
 
 
-		// Watch files
+    // Minify CSS
 
-		// https://github.com/gruntjs/grunt-contrib-watch
-		// https://npmjs.org/package/grunt-contrib-watch
+    // https://github.com/gruntjs/grunt-contrib-cssmin
+    // https://npmjs.org/package/grunt-contrib-cssmin
 
-		watch: {
-			options: {
-				livereload: true,
-				reload: true
-			},
-			sass: {
-				files: ['**/*.scss'],
-				tasks: ['sass:all']
-			}
-		}
+    cssmin: {
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['**/*.css'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
 
-	});
 
-	// Loads Grunt plugins
-	grunt.loadNpmTasks('grunt-notify');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    // Watch files
 
-	// Register Grunt tasks
-	grunt.registerTask('default', [
-		'sass:all'
-	]);
+    // https://github.com/gruntjs/grunt-contrib-watch
+    // https://npmjs.org/package/grunt-contrib-watch
 
-	grunt.registerTask('build', [
-		'sass:all',
-		'cssmin:all'
-	]);
+    watch: {
+      options: {
+        livereload: true,
+        reload: true
+      },
+      sass: {
+        files: ['**/*.scss'],
+        tasks: ['sass:all']
+      }
+    }
+
+  });
+
+  // Loads Grunt plugins
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register Grunt tasks
+  grunt.registerTask('default', [
+    'sass:all'
+  ]);
+
+  grunt.registerTask('build', [
+    'sass:all',
+    'cssmin:all'
+  ]);
 
 };
